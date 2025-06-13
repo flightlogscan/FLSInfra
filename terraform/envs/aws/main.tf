3e8ea08376631ce264add8e72ef3a80415aa4e0a
wilbur <wilbur777@protonmail.com>
2025-06-12 21:59:39 -0700

    remove unncessary var

--- terraform/envs/aws/main.tf
+++ terraform/envs/aws/main.tf
@@ -7,8 +7,22 @@ data "terraform_remote_state" "prod" {
   }
 }
 
+locals {
+  ip_list = data.terraform_remote_state.prod.outputs.prod_server_ipv4_list
+
+  valid_ip_list = length(local.ip_list) > 0 ? local.ip_list : []
+}
+
+resource "null_resource" "fail_if_empty" {
+  count = length(local.ip_list) == 0 ? 1 : 0
+
+  provisioner "local-exec" {
+    command = "echo 'ERROR: No Hetzner IPs found in prod remote state.' && exit 1"
+  }
+}
+
 module "aws_infra" {
   source                 = "../../modules/aws"
   domain_name            = "flightlogscan.com."
-  hetzner_server_ip_list = data.terraform_remote_state.prod.outputs.prod_server_ipv4_list
+  hetzner_server_ip_list = local.valid_ip_list
 }
--- terraform/envs/aws/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "hetzner_server_ip_list" {
-  type        = list(string)
-  description = "List of Hetzner server IPv4 addresses passed from CI"
-}
